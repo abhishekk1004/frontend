fdaf0e9bd6672b62c9976564da8360ac2e00a3a2
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2026-09-05 11:20:45 +0545

    fixing

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -79,8 +79,7 @@ const Resume = () => {
             </div>
           </div>
 
-          {/* ================================================================
-              SKILLS GRID */}
+          {/* SKILLS GRID */}
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
             {skills.map((skillGroup, index) => (
               <motion.div
