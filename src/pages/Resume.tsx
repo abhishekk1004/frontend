8574fd71bd804666a88bc962ebd4d5bd2c5a3ada
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2026-09-07 11:20:45 +0545

    fixing

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -131,8 +131,7 @@ const Resume = () => {
             </div>
           </div>
 
-          {/* ================================================================
-              CTA - VIEW FULL RESUME*/}
+          {/* CTA - VIEW FULL RESUME*/}
           <motion.div
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
